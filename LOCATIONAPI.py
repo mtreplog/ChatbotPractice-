eb39807e74290042d85142daf4e90e6a73e5aa7a
mtreplog <m.replogle@sap.com>
2019-03-11 15:38:14 -0400

    update

--- LOCATIONAPI.py
+++ LOCATIONAPI.py
@@ -2,11 +2,16 @@ from flask import Flask, request, jsonify
 import json
 import requests
 from KEY import API_key
-import imghdr
 import googlemaps
+import os
+import sapcai
+
 
 app = Flask(__name__)
 port = '5000'
+
+client = sapcai.Client('deaf5a1884c93db71f9d487d02b935e7')
+gmaps = googlemaps.Client(key=API_key)
 
 
 @app.route('/', methods=['POST'])
@@ -20,21 +25,27 @@ def index():
     for restaurant in restaurants:
         final.append(restaurant['name'])
     picture_reference = t.json()['results'][0]['photos'][0]['photo_reference']
-    photo_request = requests.get(
-        'https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference='+picture_reference+'&key='+API_key)
-    query = 'https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=' + \
-        picture_reference+'&key='+API_key
-    photo_type = imghdr.what("", photo_request.content)
+    raw_image_data = gmaps.places_photo(
+        photo_reference=picture_reference, max_height=40, max_width=40)
+
+    f = open("googleimage.png", "wb")
+
+    for i in raw_image_data:
+        if i:
+            f.write(i)
+
+    f.close()
     # path = "C:\Users\I506992\Desktop\" + "static\ "
     #photo_name = path.trim() + query + '.' + photo_type
 
     # with open(photo_name, "wb") as photo:
     # photo.write(photo_request.content)
+
     return jsonify(
         status=200,
         replies=[{
             'type': 'text',
-            'content': photo_type
+            'content': "{}\googleimage.png".format(os.getcwd()),
         }]
 
 
