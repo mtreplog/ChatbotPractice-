da62f875ab3904d9d25864c877a5826374fcd3da
mtreplog <m.replogle@sap.com>
2019-04-01 15:14:58 -0400

    update

--- CostObject.py
+++ CostObject.py
@@ -7,12 +7,15 @@ port = '5000'
 
 @app.route('/', methods=['POST'])
 def index():
-    print(json.loads(request.get_data()))
+    data = (json.loads(request.get_data()))
+    org_unit = data['conversation']['memory']['Org_unit']['value']
+    cost = data['conversation']['memory']['number']['value']
+    cost_type = data['conversation']['memory']['costtype']['value']
     return jsonify(
         status=200,
         replies=[{
             'type': 'text',
-            'content': 'Roger that',
+            'content': str(org_unit) + ', ' + str(cost) + ', ' + str(cost_type)
         }],
         conversation={
             'memory': {'key': 'value'}
