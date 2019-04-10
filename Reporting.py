e4cd512ae119d60fb832b30f3f0526af1e2e4a8e
mtreplog <m.replogle@sap.com>
2019-04-10 15:46:37 -0400

    finish CC update payload

--- Reporting.py
+++ Reporting.py
@@ -10,13 +10,13 @@ Org_Dict = {'oss': 'Operations & Shared Service', 'hr2': 'HR II', 'pr': 'Procure
             'iam': 'Identity & Accessmanagement', 's/4': 'IT S/4 HANA Program Office', 'app': 'IT Application Services Mgmt', 'cross': 'Cross IT & Operations Management', 'hr': 'HR I', 'cont': 'Controlling', 'corpfin': 'Coprporate Finance Mgmt', 'shared': 'Shared IT Applications', 'dummy': 'Dummy'}
 
 Del_dict = {1: 'IT Contract to Revenue', 2: 'Cross IT & Operations', 3: 'IT Human Resources', 4: 'IT Corporate Finance',
-            5: 'IT Services, Entitlement & Delivery', 6: 'IT Application Services Mgmt', 7: 'IT S/4 HANA Program Office', 8: 'IT Go-To-Market Services'}
+            5: 'IT Services, Entitlement & Delivery', 6: 'IT Application Services Mgmt', 7: 'IT S/4 HANA Program Office', 8: 'IT Go-To-Market Services', 9: 'Dummy'}
 Node_dict = {1: 'M3CIT03008', 2: 'M3CIT03003', 3: 'M3CIT03009', 4: 'M2CIT00302',
-             5: 'M3CIT03010', 6: 'M3ITIN0206', 7: 'M3CIT04054', 8: 'M2CIT00312'}
+             5: 'M3CIT03010', 6: 'M3ITIN0206', 7: 'M3CIT04054', 8: 'M2CIT00312', 9: 'M3456'}
 Cost_Center_ID = {'oss': '108611700', 'hr2': '115000402',
-                  'pr': '101035030', 'iam': '101000452', 's/4': '101004059', 'app': '108004071'}
+                  'pr': '101035030', 'iam': '101000452', 's/4': '101004059', 'app': '108004071', 'cross': '108004036', 'hr': '101004080', 'cont': '181010088', 'corpfin': '101030010', 'shared': '176223020', 'dummy': '11238769'}
 Cost_Center_Dict = {'oss': 'IT Ops & SharServ US', 'hr2': 'IT HR II SGD',
-                    'pr': 'IT Procurement SE', 'iam': 'M3CIT03003', 's/4': 'IT S/4 Hana PO SE', 'app': 'IT AppServ Mgmt US'}
+                    'pr': 'IT Procurement SE', 'iam': 'M3CIT03003', 's/4': 'IT S/4 Hana PO SE', 'app': 'IT AppServ Mgmt US', 'cross': 'Cross IT & Ops Mg US', 'hr': 'IT HR I SE', 'cont': 'IT Controlling ROM', 'corpfin': 'IT Fin Serv Mgmt SE', 'shared': 'SharIT App IND', 'dummy': 'Dummy IT APP'}
 
 
 @app.route('/mike', methods=['POST'])
@@ -204,14 +204,16 @@ def index2():
     elif org_unit == 'shared':
         org_org = 2
     elif org_unit == 'dummy':
-        org_org = 2
+        org_org = 9
     payload['CostCenter']['DeliveryUnit']['OrganizationalUnit']['OU'] = Del_dict[org_org]
     payload[cleandate] = cleancost
     payload['Resource']['Name'] = cleanname
     payload['Resource']['UserID'] = cleanid
-    payload['CostCenter']['DeliveryUnit']['DeliveryUnit']['DU'] = cleanDU
+    payload['CostCenter']['DeliveryUnit']['DU'] = cleanDU
     payload['CostCenter']['DeliveryUnit']['OrganizationalUnit']['Node'] = Node_dict[org_org]
     payload['CostGroup']['CostGroup'] = cleantype
+    payload['CostCenter']['CCID'] = Cost_Center_ID[org_unit]
+    payload['CostCenter']['Name'] = Cost_Center_Dict[org_unit]
     post = requests.post(
         'https://cost-center-management-production2.cfapps.eu10.hana.ondemand.com/rest/restplanning/v1/Planning', json=payload, headers=headers)
     return jsonify(
